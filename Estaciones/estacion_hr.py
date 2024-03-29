bb9dba84fec6f0ad7ec0040c3cc926717abb2baf
Granada <granadalexi@gmail.com>
2024-03-29 22:06:53 +0000

    VirtualMachine

---
+++ Estaciones/estacion_hr.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 16:33:48 2024
+Exploración de temperatura de las estaciones del IDEAM
+@author: Arturo A. Granada g.
+"""
+
+'''Importamos librerias'''
+import pandas as pd #Leer datos
+import plotly.express as px
+import plotly.graph_objects as go
