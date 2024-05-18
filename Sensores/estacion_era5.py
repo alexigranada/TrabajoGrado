a72e6f55ddc29e9e4870334decb4b07f7f46085d
UVGranada <granadalexi@gmail.com>
2024-05-17 20:34:10 -0500

    Mayo-17-2024U

--- Sensores/estacion_era5.py
+++ Sensores/estacion_era5.py
@@ -13,16 +13,16 @@ import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt #Graficas y limpiar datos
 
-e5 = 'Datos/ERA5/UPacifico/ERA5_UPacifico_Hora.csv' #Ruta del archivo CHIRPS
-es = 'Datos/Hora/V_Climaticas_LaCumbre_RL_Hora.csv' #Ruta del archivo Estatción
+ruta_e5 = 'Datos/ERA5/LaCumbre/ERA5_LaCumbre_Hora.csv' #Ruta del archivo ERA5
+ruta_estacion = 'Datos/Hora/V_Climaticas_LaCumbre_RL_Hora.csv' #Ruta del archivo Estatción
 
-era5 = pd.read_csv(e5, delimiter=';') #Cargamos archivo
-es = pd.read_csv(es, delimiter=';')
+era5 = pd.read_csv(ruta_e5, delimiter=';') #Cargamos archivo
+estacion = pd.read_csv(ruta_estacion, delimiter=';')
 print(era5)
 #print(dch)
 ''' Creamos el formato de la fecha'''
 era5['datetime'] = pd.to_datetime(era5['datetime'], format='%Y-%m-%d %H:%M:%S')
-es['Fecha (UTC-05:00)'] = pd.to_datetime(es['Fecha (UTC-05:00)'], format='%d/%m/%Y %H:%M')
+estacion['Fecha (UTC-05:00)'] = pd.to_datetime(estacion['Fecha (UTC-05:00)'], format='%d/%m/%Y %H:%M')
 
 '''Sumamos la precipitación por día en la estación'''
 #es.set_index('Fecha (UTC-05:00)', inplace=True)
@@ -32,16 +32,23 @@ es['Fecha (UTC-05:00)'] = pd.to_datetime(es['Fecha (UTC-05:00)'], format='%d/%m/
 fig = go.Figure()
 
 temp = era5['temperature_2m']
-tes = es['Temp Media']
-time = es['Fecha (UTC-05:00)']
+tes = estacion['Temp Media']
+time = estacion['Fecha (UTC-05:00)'][:120]
 
 #fig = px.bar(dch, x='Fecha', y='prcp', color_discrete_sequence=['darkblue']) #['blue'] - px.colors.qualitative.Dark24
-fig.add_trace(go.Scatter(x=time, y=temp, mode='lines', name='Temperatura ERA5', line=dict(color='#2ECC71'))) #2ECC71
-fig.add_trace(go.Scatter(x=time, y=tes, mode='lines', name='Temperatura Estación', line=dict(color='#F44336')))
+fig.add_trace(go.Scatter(x=time, y=temp, mode='lines', name='Temperatura ERA5' )) #2ECC71 line=dict(color='#2ECC71'))
+fig.add_trace(go.Scatter(x=time, y=tes, mode='lines', name='Temperatura Estación')) #line=dict(color='#F44336'))
+
 title = f'Temperatura ERA5 vs Estación La Cumbre'
-fig.update_layout(title = title,
-                  xaxis = dict(title='Día'),
-                  yaxis = dict(title='Temperatura (Celsius)'),
-                  template = 'plotly_white',
+fig.update_layout(title=title,
+                  xaxis = dict(title='Tiempo (Hora)'),
+                  yaxis = dict(title='Temperatura (°C)'),
+                  template = 'seaborn', #'plotly_white' - 'plotly_dark' - 'ggplot2' - 'seaborn - 'simple_white'
+                  title_font_size=22,
                   title_x = 0.5)
 fig.show()
+fig.write_image("Patron_temperatura_era5_lacumbre.png", width=800, height=500, scale=4)
+#title_font_family=
+estacion_nulos = estacion['Temp Media'].dropna()
+correlacion = estacion_nulos.corr(temp)
+print('Patron Estacion vs ERA5 (Temperatura)', correlacion)
