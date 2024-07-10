ffe10031c8ef102dff53b1e6abfd2b9ff9d44222
UVGranada <granadalexi@gmail.com>
2024-07-09 19:16:07 -0500

    Julio-9-2024U

--- AED/correlacion_variables_rs.py
+++ AED/correlacion_variables_rs.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Jul 08 12:46:55 2024
 
-Correlacion entre las variables climáticas a modelar
+Correlacion entre las variables climáticas Radiación solar con las variables a modelar
 
 @author: Arturo A. Granada G.
 """
@@ -16,23 +16,22 @@ import plotly.graph_objects as go
 
 
 r0 = 'Datos/ERA5_Land_T_P/ERA5-Land_2015_2023_ValleDelCauca_3H.nc'
-r1 = 'Datos/GCM/GFDL SSP 126/tas_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010300-203501010000_ValleDelCauca.nc'
+r1 = 'Datos/GCM/GFDL SSP 126/rlds_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010130-203412312230_ValleDelCauca.nc'
+r3 = 'Datos/GCM/GFDL SSP 126/rlus_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010130-203412312230_ValleDelCauca.nc'
+r4 = 'Datos/GCM/GFDL SSP 126/rsds_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010130-203412312230_ValleDelCauca.nc'
+r5 = 'Datos/GCM/GFDL SSP 126/rsus_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010130-203412312230_ValleDelCauca.nc'
+#r6 = 'Datos/GCM/GFDL SSP 126/uas_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010300-203501010000_ValleDelCauca.nc'
+#r7 = 'Datos/GCM/GFDL SSP 126/vas_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010300-203501010000_ValleDelCauca.nc'
 #2 = 'Datos/GCM/GFDL SSP 126/pr_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010130-203412312230_ValleDelCauca.nc'
-r3 = 'Datos/GCM/GFDL SSP 126/huss_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010300-203501010000_ValleDelCauca.nc'
-r4 = 'Datos/GCM/GFDL SSP 126/mrsos_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010300-203501010000_ValleDelCauca.nc'
-r5 = 'Datos/GCM/GFDL SSP 126/ps_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010300-203501010000_ValleDelCauca.nc'
-r6 = 'Datos/GCM/GFDL SSP 126/uas_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010300-203501010000_ValleDelCauca.nc'
-r7 = 'Datos/GCM/GFDL SSP 126/vas_3hr_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501010300-203501010000_ValleDelCauca.nc'
 
 ds0 = xr.open_dataset(r0)
 ds1 = xr.open_dataset(r1)
-#ds2 = xr.open_dataset(r2)
 ds3 = xr.open_dataset(r3)
 ds4 = xr.open_dataset(r4)
 ds5 = xr.open_dataset(r5)
-ds6 = xr.open_dataset(r6)
-ds7 = xr.open_dataset(r7)
-##ds = xr.merge([ds1, ds3, ds4, ds5, ds6, ds7], compat='override')
+#ds6 = xr.open_dataset(r6)
+#ds7 = xr.open_dataset(r7)
+#ds2 = xr.open_dataset(r2)
 
 ''' Selecionamos Pixel de interes del GCM'''
 lat = 3.5
@@ -41,8 +40,6 @@ ds1 = ds1.sel(lon = lon, lat = lat, method='nearest')
 ds3 = ds3.sel(lon = lon, lat = lat, method='nearest')
 ds4 = ds4.sel(lon = lon, lat = lat, method='nearest')
 ds5 = ds5.sel(lon = lon, lat = lat, method='nearest')
-ds6 = ds6.sel(lon = lon, lat = lat, method='nearest')
-ds7 = ds7.sel(lon = lon, lat = lat, method='nearest')
 
 ''' Seleccionamos el pixel para cada una de las estaciones'''
 lon_pacifico, lat_pacifico = -76.5647 , 3.8480
@@ -60,13 +57,10 @@ aBonilla = ds0.sel(longitude = lon_aBonilla, latitude = lat_aBonilla, method='ne
 diana = ds0.sel(longitude = lon_diana, latitude = lat_diana, method='nearest')
 
 ''' Seleccionamos variables GCM'''
-tas = ds1['tas']
-##pr = ds2['pr']
-huss = ds3['huss']
-mrsos = ds4['mrsos']
-ps = ds5['ps']
-uas = ds6['uas']
-vas = ds7['vas']
+rlds = ds1['rlds']
+rlus = ds3['rlus']
+rsds = ds4['rsds']
+rsus = ds5['rsus']
 
 ''' Seleccionamos variables ERA5-Land'''
 pacifico_t2m = pacifico['t2m']
@@ -82,12 +76,10 @@ f2 = '2035-01-01 00:00:00'
 fgregoriano = pd.date_range(f1, f2, freq='3h')
 fecha = fgregoriano[~((fgregoriano.month == 2) & (fgregoriano.day == 29))]
 
-tas['time'] = fecha
-huss['time'] = fecha
-mrsos['time'] = fecha
-ps['time'] = fecha
-uas['time'] = fecha
-vas['time'] = fecha
+rlds['time'] = fecha
+rlus['time'] = fecha
+rsds['time'] = fecha
+rsus['time'] = fecha
 
 ''' Asignamos la Fecha para ERA5-Land sin días bisiestos'''
 pacifico_t2m = pacifico_t2m.loc['2015-01-01 03:00':'2023-12-31 21:00']
@@ -105,13 +97,10 @@ aBonilla_t2m_era = aBonilla_t2m.loc[~((aBonilla_t2m['time'].dt.month == 2) & (aB
 diana_t2m_era = diana_t2m.loc[~((diana_t2m['time'].dt.month == 2) & (diana_t2m['time'].dt.day == 29))]
 
 ''' Convertimos a DF'''
-df_tas = tas.to_dataframe().reset_index()
-##df_pr = pr.to_dataframe().reset_index()
-df_huss = huss.to_dataframe().reset_index()
-df_mrsos = mrsos.to_dataframe().reset_index()
-df_ps = ps.to_dataframe().reset_index()
-df_uas = uas.to_dataframe().reset_index()
-df_vas = vas.to_dataframe().reset_index()
+df_rlds = rlds.to_dataframe().reset_index()
+df_rlus = rlus.to_dataframe().reset_index()
+df_rsds = rsds.to_dataframe().reset_index()
+df_rsus = rsus.to_dataframe().reset_index()
 
 df_pacifico = pacifico_t2m_era.to_dataframe().reset_index()
 df_cumbre = cumbre_t2m_era.to_dataframe().reset_index()
@@ -121,12 +110,10 @@ df_aBonilla = aBonilla_t2m_era.to_dataframe().reset_index()
 df_diana = diana_t2m_era.to_dataframe().reset_index()
 
 ''' Creamos la fecha como indice'''
-df_tas.set_index('time', inplace=True)
-df_huss.set_index('time', inplace=True)
-df_mrsos.set_index('time', inplace=True)
-df_ps.set_index('time', inplace=True)
-df_uas.set_index('time', inplace=True)
-df_vas.set_index('time', inplace=True)
+df_rlds.set_index('time', inplace=True)
+df_rlus.set_index('time', inplace=True)
+df_rsds.set_index('time', inplace=True)
+df_rsus.set_index('time', inplace=True)
 
 df_pacifico.set_index('time', inplace=True)
 df_cumbre.set_index('time', inplace=True)
@@ -139,66 +126,39 @@ df_diana.set_index('time', inplace=True)
 f_i = '2015-01-01 03:00:00'
 f_f = '2023-12-31 21:00:00'
 
-df_tas = df_tas.loc[f_i:f_f]
-df_huss = df_huss.loc[f_i:f_f]
-df_mrsos = df_mrsos.loc[f_i:f_f]
-df_ps = df_ps.loc[f_i:f_f]
-df_uas = df_uas.loc[f_i:f_f]
-df_vas = df_vas.loc[f_i:f_f]
+df_rlds = df_rlds.loc[f_i:f_f]
+df_rlus = df_rlus.loc[f_i:f_f]
+df_rsds = df_rsds.loc[f_i:f_f]
+df_rsus = df_rsus.loc[f_i:f_f]
 
-#print(df_tas)
+#print(df_rlds)
 #print(df_pacifico)
-''' Matrix de correlación'''
-#features = ['tas', 'huss', 'mrsos', 'ps', 'uas', 'vas']
-#fig = px.scatter_matrix(df, dimensions=features)
-#fig.update_traces(diagonal_visible=False)
-#fig.show()
 
 ''' Correlación'''
-print('Correlación tas')
-correlacion, p_value = spearmanr(df_diana['t2m'], df_tas['tas'])
+print('Correlación rlds')
+correlacion, p_value = spearmanr(df_pacifico['t2m'], df_rlds['rlds'])
 print(f'Correlación Spearmanr: {correlacion:.3f}, P-value: {p_value:.3f}')
 
-correlacion, p_value = pearsonr(df_diana['t2m'], df_tas['tas'])
+correlacion, p_value = pearsonr(df_pacifico['t2m'], df_rlds['rlds'])
 print(f'Correlación Pearson: {correlacion:.3f}, P-value: {p_value:.3f}')
 
-print('Correlación huss')
-correlacion, p_value = spearmanr(df_diana['t2m'], df_huss['huss'])
+print('Correlación rlus')
+correlacion, p_value = spearmanr(df_pacifico['t2m'], df_rlus['rlus'])
 print(f'Correlación Spearmanr: {correlacion:.3f}, P-value: {p_value:.3f}')
 
-correlacion, p_value = pearsonr(df_diana['t2m'], df_huss['huss'])
+correlacion, p_value = pearsonr(df_pacifico['t2m'], df_rlus['rlus'])
 print(f'Correlación Pearson: {correlacion:.3f}, P-value: {p_value:.3f}')
 
-print('Correlación mrsos')
-correlacion, p_value = spearmanr(df_diana['t2m'], df_mrsos['mrsos'])
+print('Correlación rsds')
+correlacion, p_value = spearmanr(df_pacifico['t2m'], df_rsds['rsds'])
 print(f'Correlación Spearmanr: {correlacion:.3f}, P-value: {p_value:.3f}')
 
-correlacion, p_value = pearsonr(df_diana['t2m'], df_mrsos['mrsos'])
+correlacion, p_value = pearsonr(df_pacifico['t2m'], df_rsds['rsds'])
 print(f'Correlación Pearson: {correlacion:.3f}, P-value: {p_value:.3f}')
 
-print('Correlación ps')
-correlacion, p_value = spearmanr(df_diana['t2m'], df_ps['ps'])
+print('Correlación rsus')
+correlacion, p_value = spearmanr(df_pacifico['t2m'], df_rsus['rsus'])
 print(f'Correlación Spearmanr: {correlacion:.3f}, P-value: {p_value:.3f}')
 
-correlacion, p_value = pearsonr(df_diana['t2m'], df_ps['ps'])
+correlacion, p_value = pearsonr(df_pacifico['t2m'], df_rsus['rsus'])
 print(f'Correlación Pearson: {correlacion:.3f}, P-value: {p_value:.3f}')
-
-print('Correlación uas')
-correlacion, p_value = spearmanr(df_diana['t2m'], df_uas['uas'])
-print(f'Correlación Spearmanr: {correlacion:.3f}, P-value: {p_value:.3f}')
-
-correlacion, p_value = pearsonr(df_diana['t2m'], df_uas['uas'])
-print(f'Correlación Pearson: {correlacion:.3f}, P-value: {p_value:.3f}')
-
-print('Correlación vas')
-correlacion, p_value = spearmanr(df_diana['t2m'], df_vas['vas'])
-print(f'Correlación Spearmanr: {correlacion:.3f}, P-value: {p_value:.3f}')
-
-correlacion, p_value = pearsonr(df_diana['t2m'], df_vas['vas'])
-print(f'Correlación Pearson: {correlacion:.3f}, P-value: {p_value:.3f}')
-
-
-
-
-
-
